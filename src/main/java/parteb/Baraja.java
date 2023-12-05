8e088eca5870d14e852253afa3c655f95e248662
Daniel <dnavbor167@g.educaand.es>
2023-12-05 20:53:56 +0100

    Creación de Parte B, atributos encapsulados y comienzo del primer constructor

---
+++ src/main/java/parteb/Baraja.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parteb;
+
+import partea.Naipe;
+
+/**
+ *
+ * @author danielnavasborjas
+ */
+public class Baraja {
+    //Definimos los atributos encapsulados
+    private final Naipe[] naipeArray = new Naipe[barajasTienen];
+    public static final int barajasTienen = 40;
+
+    //Creamos el constructor sin parámetros
+    public Baraja(Naipe[] naipeArray) {
+        
+        //Creamos la lógica para mostrar las 40 cartas sin que se repitan
+        for(int i = 1;i<=10;i++){
+            for(int a = 1;a<=3;a++){
+                this.naipeArray[]=;
+            }
+        }
+    }
+    
+    
+    
+    
+}
