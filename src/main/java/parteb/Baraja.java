40a41dfd29ca425171bd318d994301767fa96f81
Daniel <dnavbor167@g.educaand.es>
2023-12-06 20:51:16 +0100

    Creación de Baraja(Los 4 primeros puntos)

--- src/main/java/parteb/Baraja.java
+++ src/main/java/parteb/Baraja.java
@@ -5,28 +5,45 @@
 package parteb;
 
 import partea.Naipe;
+import partea.Palo;
 
 /**
  *
  * @author danielnavasborjas
  */
 public class Baraja {
+
     //Definimos los atributos encapsulados
     private final Naipe[] naipeArray = new Naipe[barajasTienen];
     public static final int barajasTienen = 40;
+    private Palo[] paloArray = Palo.values();
 
     //Creamos el constructor sin parámetros
-    public Baraja(Naipe[] naipeArray) {
-        
+    public Baraja() {
+
+        int contador = 0;
         //Creamos la lógica para mostrar las 40 cartas sin que se repitan
-        for(int i = 1;i<=10;i++){
-            for(int a = 1;a<=3;a++){
-                this.naipeArray[]=;
+        for (int i = 1; i <= 10; i++) {
+            for (int a = 0; a < paloArray.length; a++) {
+                this.naipeArray[contador++] = new Naipe(i, paloArray[a]);
             }
         }
     }
+
+    //Definimos el getter
+    public Naipe[] getNaipeArray() {
+        return naipeArray;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Baraja{");
+        sb.append("naipeArray=").append(naipeArray);
+        sb.append('}');
+        return sb.toString();
+    }
+
     
-    
-    
-    
+
 }
