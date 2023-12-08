69367bd0303c2ef99a3010159133c7df5fb08883
Daniel <dnavbor167@g.educaand.es>
2023-12-08 13:36:34 +0100

    Creación del método barajar(int movimientos)

--- src/main/java/parteb/Baraja.java
+++ src/main/java/parteb/Baraja.java
@@ -4,6 +4,7 @@
  */
 package parteb;
 
+import java.util.Random;
 import partea.Naipe;
 import partea.Palo;
 
@@ -35,6 +36,7 @@ public class Baraja {
         return naipeArray;
     }
 
+    //Creamos el toString
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -44,6 +46,37 @@ public class Baraja {
         return sb.toString();
     }
 
-    
+    //Método que recibe como parámetro un int indicando los movimientos que va
+    //a tener la carta y en cada movimiento se elegirán dos cartas aleatorias y
+    //se intercambiarán sus posciones dentro del array
+    public static void barajar(int movimientos){
+        
+        //Instanciamos el objeto Baraja
+        Baraja baraja = new Baraja();
+        Random r = new Random();
+        
+        for(int i = 1; i <= movimientos;i++){
+            
+            //Guardamos en dos variables de tipo int los valores aleatorios
+            //que tiene la longitud de la baraja
+            int posicion1 = r.nextInt(barajasTienen);
+            int posicion2 = r.nextInt(barajasTienen);
+            
+            //Creamos dos objetos de Naipe asignándole el valor aleatorio de 
+            //las posiciones
+            Naipe np1 = baraja.getNaipeArray()[posicion1];
+            Naipe np2 = baraja.getNaipeArray()[posicion2];
+            
+            //Intercambio de cartas
+            Naipe inter = np1;
+            np1 = np2;
+            np2 = inter;
+            
+            //Mostramos el valor del intercambio
+            System.out.println("Movimiento " + i + "\nNaipe1 = " + np1 + 
+                    "\nNaipe2 = " + np2);
+        }
+        
+    }
 
 }
